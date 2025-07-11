e7b878572b09b11a3eee727bc48ef48793488495
Kharbachzoubair <zoubairkharbach@gmail.com>
2025-07-11 20:49:13 +0100

    done

--- main.c
+++ main.c
@@ -6,35 +6,233 @@
 /*   By: zkharbac <zkharbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 12:52:39 by zkharbac          #+#    #+#             */
-/*   Updated: 2025/07/06 13:11:16 by zkharbac         ###   ########.fr       */
+/*   Updated: 2025/07/11 16:05:42 by zkharbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
+int is_digit_str(const char *str)
 {
-	t_data	data;
+	int i = 0;
+	if (!str || !str[0])
+		return (0);
+	while (str[i])
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		else
+			i++;
+	return (1);
+}
+
+int safe_atoi(const char *str, int *result)
+{
+	long num = 0;
+	int i = 0;
+	if (!is_digit_str(str))
+		return (0);
+	while (str[i])
+	{
+		num = num * 10 + (str[i] - '0');
+		if (num > INT_MAX)
+			return (0);
+		i++;
+	}
+	*result = (int)num;
+	return (1);
+}
+
+long long get_time(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+void ft_usleep(long long time, t_data *data)
+{
+	long long start = get_time();
+	while (get_time() - start < time)
+	{
+		pthread_mutex_lock(&data->death_mutex);
+		if (data->someone_died)
+		{
+			pthread_mutex_unlock(&data->death_mutex);
+			break;
+		}
+		pthread_mutex_unlock(&data->death_mutex);
+		usleep(100);
+	}
+}
+
+/* ************************************************************************** */
+
+// args.c
+#include "philo.h"
+
+int parse_args(int argc, char **argv, t_data *data)
+{
+	if (argc != 5 && argc != 6)
+		return (0);
+	if (!safe_atoi(argv[1], &data->nb_philos) || data->nb_philos <= 0)
+		return (0);
+	if (!safe_atoi(argv[2], &data->time_to_die) || data->time_to_die <= 0)
+		return (0);
+	if (!safe_atoi(argv[3], &data->time_to_eat) || data->time_to_eat <= 0)
+		return (0);
+	if (!safe_atoi(argv[4], &data->time_to_sleep) || data->time_to_sleep <= 0)
+		return (0);
+	if (argc == 6)
+	{
+		if (!safe_atoi(argv[5], &data->must_eat) || data->must_eat <= 0)
+			return (0);
+		data->has_optional_arg = 1;
+	}
+	else
+		data->has_optional_arg = 0;
+	return (1);
+}
+
+/* ************************************************************************** */
+
+// main.c
+#include "philo.h"
+
+int main(int argc, char **argv)
+{
+	t_data data;
 
 	if (!parse_args(argc, argv, &data))
 	{
 		printf("Error: Invalid arguments.\n");
-		printf("Usage: ./philo number_of_philosophers time_to_die ");
-		printf("time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
 		return (1);
 	}
-
-	// Debug print (remove later)
-	printf("Philosophers: %d\n", data.nb_philos);
-	printf("Time to die: %d\n", data.time_to_die);
-	printf("Time to eat: %d\n", data.time_to_eat);
-	printf("Time to sleep: %d\n", data.time_to_sleep);
-	if (data.has_optional_arg)
-		printf("Must eat: %d times\n", data.must_eat);
-	else
-		printf("No meal limit.\n");
-
-	// Continue to init forks, create threads, etc.
-
+	if (init_simulation(&data))
+		return (1);
+	cleanup(&data);
 	return (0);
 }
+
+/* ************************************************************************** */
+
+// init.c
+#include "philo.h"
+
+int init_simulation(t_data *data)
+{
+	int i;
+
+	data->someone_died = 0;
+	data->start_time = get_time();
+	pthread_mutex_init(&data->print_mutex, NULL);
+	pthread_mutex_init(&data->death_mutex, NULL);
+	pthread_mutex_init(&data->start_mutex, NULL);
+
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->nb_philos);
+	data->philos = malloc(sizeof(t_philo) * data->nb_philos);
+	if (!data->forks || !data->philos)
+		return (1);
+	i = -1;
+	while (++i < data->nb_philos)
+		pthread_mutex_init(&data->forks[i], NULL);
+	i = -1;
+	while (++i < data->nb_philos)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].meals = 0;
+		data->philos[i].data = data;
+		data->philos[i].left_fork = &data->forks[i];
+		data->philos[i].right_fork = &data->forks[(i + 1) % data->nb_philos];
+		data->philos[i].last_meal = get_time();
+		if (pthread_create(&data->philos[i].thread, NULL, philo_routine, &data->philos[i]))
+			return (1);
+	}
+	return (0);
+}
+
+void cleanup(t_data *data)
+{
+	int i = -1;
+	while (++i < data->nb_philos)
+		pthread_join(data->philos[i].thread, NULL);
+	i = -1;
+	while (++i < data->nb_philos)
+		pthread_mutex_destroy(&data->forks[i]);
+	pthread_mutex_destroy(&data->print_mutex);
+	pthread_mutex_destroy(&data->death_mutex);
+	pthread_mutex_destroy(&data->start_mutex);
+	free(data->forks);
+	free(data->philos);
+}
+
+/* ************************************************************************** */
+
+// routine.c
+#include "philo.h"
+
+void take_forks(t_philo *philo)
+{
+	pthread_mutex_lock(philo->left_fork);
+	print_status(philo, "has taken a fork");
+	pthread_mutex_lock(philo->right_fork);
+	print_status(philo, "has taken a fork");
+}
+
+void drop_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_unlock(philo->left_fork);
+}
+
+void print_status(t_philo *philo, char *msg)
+{
+	pthread_mutex_lock(&philo->data->death_mutex);
+	if (!philo->data->someone_died)
+	{
+		pthread_mutex_lock(&philo->data->print_mutex);
+		printf("%lld %d %s\n", get_time() - philo->data->start_time, philo->id, msg);
+		pthread_mutex_unlock(&philo->data->print_mutex);
+	}
+	pthread_mutex_unlock(&philo->data->death_mutex);
+}
+
+void *philo_routine(void *arg)
+{
+	t_philo *philo = (t_philo *)arg;
+
+	if (philo->data->nb_philos == 1)
+		return (one_philo(philo), NULL);
+	if (philo->id % 2 == 0)
+		usleep(100);
+	while (1)
+	{
+		pthread_mutex_lock(&philo->data->death_mutex);
+		if (philo->data->someone_died)
+		{
+			pthread_mutex_unlock(&philo->data->death_mutex);
+			break;
+		}
+		pthread_mutex_unlock(&philo->data->death_mutex);
+		take_forks(philo);
+		print_status(philo, "is eating");
+		pthread_mutex_lock(&philo->data->death_mutex);
+		philo->last_meal = get_time();
+		philo->meals++;
+		pthread_mutex_unlock(&philo->data->death_mutex);
+		ft_usleep(philo->data->time_to_eat, philo->data);
+		drop_forks(philo);
+		print_status(philo, "is sleeping");
+		ft_usleep(philo->data->time_to_sleep, philo->data);
+		print_status(philo, "is thinking");
+	}
+	return (NULL);
+}
+
+void one_philo(t_philo *philo)
+{
+	print_status(philo, "has taken a fork");
+	ft_usleep(philo->data->time_to_die, philo->data);
+	print_status(philo, "died");
+	philo->data->someone_died = 1;
+}
+
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: zkharbac <zkharbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 11:48:23 by zkharbac          #+#    #+#             */
-/*   Updated: 2025/07/06 13:12:08 by zkharbac         ###   ########.fr       */
+/*   Updated: 2025/07/11 16:05:26 by zkharbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,51 @@
 # include <stdlib.h>
 # include <limits.h>
 # include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
 
-typedef struct s_data {
-	int	nb_philos;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	must_eat;
-	int	has_optional_arg;
-}	t_data;
+typedef struct s_data t_data;
 
-// args.c
-int	parse_args(int argc, char **argv, t_data *data);
+typedef struct s_philo
+{
+	int			id;
+	int			meals;
+	long long	last_meal;
+	pthread_t	thread;
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
+	t_data		*data;
+} t_philo;
 
-// utils.c
-int	safe_atoi(const char *str, int *result);
-int	is_digit_str(const char *str);
+typedef struct s_data
+{
+	int			nb_philos;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			must_eat;
+	int			has_optional_arg;
+	int			someone_died;
+	long long	start_time;
+	pthread_mutex_t *forks;
+	pthread_mutex_t print_mutex;
+	pthread_mutex_t death_mutex;
+	pthread_mutex_t start_mutex;
+	t_philo		*philos;
+} t_data;
+
+int		parse_args(int argc, char **argv, t_data *data);
+int		safe_atoi(const char *str, int *result);
+int		is_digit_str(const char *str);
+long long get_time(void);
+void	ft_usleep(long long time, t_data *data);
+void	*philo_routine(void *arg);
+void	print_status(t_philo *philo, char *msg);
+void	take_forks(t_philo *philo);
+void	drop_forks(t_philo *philo);
+void	one_philo(t_philo *philo);
+int		init_simulation(t_data *data);
+void	cleanup(t_data *data);
 
 #endif
+
